499e43cde8c4c6639a8f61c371b75b73408ce109
unknown <alstrekozov@STD4.arteast.local>
2019-10-09 18:03:05 +0500

    Добавил 920 720 версию
    Signed-off-by: unknown <alstrekozov@STD4.arteast.local>

--- js/main.js
+++ js/main.js
@@ -3,8 +3,9 @@ let entry = document.querySelector('.header-button'),
     popUPHelp = document.querySelector('.header-help__popUP'),
     body = document.querySelector('body'),
     popUp = document.querySelector('.popUp'),
-    popUpClose = document.querySelector('.popUp-close'),
-    img = document.querySelectorAll('.payment-block__img');
+    mobilePayment = document.getElementsByClassName('payment-block__text__mobile'),
+    mobilePaymentText = document.getElementsByClassName('payment-block__text'),
+    popUpClose = document.querySelector('.popUp-close');
 
 entry.addEventListener('click', function(){
     popUp.classList.add('open');
@@ -22,41 +23,34 @@ linkHelp.addEventListener('mouseout', function(){
     popUPHelp.classList.remove('open');
 })
 
+// for (i = 0; i < mobilePayment.length; i++) {
+//   mobilePayment[i].addEventListener('click', toggleItem, false);
+
+// }
+// function toggleItem() {
+//   var itemClass = this.parentNode.className;
+//   for (i = 0; i < mobilePaymentText.length; i++) {
+//     mobilePaymentText[i].className = 'accordionItem close';
+//   }
+//   if (itemClass == 'accordionItem close') {
+//       this.parentNode.className = 'accordionItem open';
+//   }
+// }
 
 
-var Visible = function (target) {
-    // Все позиции элемента
-    var targetPosition = {
-        top: window.pageYOffset + target.getBoundingClientRect().top,
-        left: window.pageXOffset + target.getBoundingClientRect().left,
-        right: window.pageXOffset + target.getBoundingClientRect().right,
-        bottom: window.pageYOffset + target.getBoundingClientRect().bottom
-      },
-      // Получаем позиции окна
-      windowPosition = {
-        top: window.pageYOffset,
-        left: window.pageXOffset,
-        right: window.pageXOffset + document.documentElement.clientWidth,
-        bottom: window.pageYOffset + document.documentElement.clientHeight
-      };
-  
-    if (targetPosition.bottom > windowPosition.top && // Если позиция нижней части элемента больше позиции верхней чайти окна, то элемент виден сверху
-      targetPosition.top < windowPosition.bottom && // Если позиция верхней части элемента меньше позиции нижней чайти окна, то элемент виден снизу
-      targetPosition.right > windowPosition.left && // Если позиция правой стороны элемента больше позиции левой части окна, то элемент виден слева
-      targetPosition.left < windowPosition.right) { // Если позиция левой стороны элемента меньше позиции правой чайти окна, то элемент виден справа
-      // Если элемент полностью видно, то запускаем следующий код
-      console.clear();
-      console.log('Вы видите элемент :)');
+
+var arrayElem = [];
+
+for (var i = 0; i < mobilePayment.length; i++){
+  arrayElem.push(mobilePayment[i]);
+  mobilePayment[i].addEventListener('click', function(e){
+    let index = arrayElem.indexOf(e.target);
+    if (mobilePaymentText[index].className == "payment-block__text" || mobilePaymentText[index].className == "payment-block__text payment-block__text__right" || mobilePaymentText[index].className == "payment-block__text payment-block__text__left"){
+      mobilePaymentText[index].classList.add('open');
     } else {
-      // Если элемент не видно, то запускаем этот код
-      console.clear();
-    };
-  };
-  
-  // Запускаем функцию при прокрутке страницы
-  window.addEventListener('scroll', function() {
-    Visible (img[1]);
+      mobilePaymentText[index].classList.remove('open');
+    }
+      
+     console.log(index);
   });
-  
-  // А также запустим функцию сразу. А то вдруг, элемент изначально видно
-  Visible (img[1]);
+}
